d4fe8684fc652d280d7f50c9edd84c4008b47f2b
Nelly31 <nelly_main@hotmail.com>
2019-07-25 16:51:39 +0000

    add db connection for user details

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,19 +17,28 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
+
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  console.log('getting the user');
+  return pool.query(`
+  SELECT * FROM users
+  WHERE email = $1;
+  `, [email])
+    .then(res => {
+      const [user] = res.rows;
+
+      if (user) {
+        return user;
+      } else
+        return null;
+    })
+    .catch(error => {
+      return error;
+    });
 };
 exports.getUserWithEmail = getUserWithEmail;
+
 
 /**
  * Get a single user from the database given their id.
@@ -37,7 +46,18 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT * FROM users
+  WHERE id = $1;
+  `, [id])
+    .then(res => {
+      const [user] = res.rows;
+
+      if (user) {
+        return user;
+      } else
+        return null;
+    });
 };
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +68,10 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  let userInfo = [user.name, user.email, user.password];
+  console.log(userInfo);
+  return pool.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, userInfo)
+    .then(res => res.rows);
 };
 exports.addUser = addUser;
 
